26d4b2d70cc05af9438b72c983f3b051e4dbdb0d
spatangi <44306230+spatangi@users.noreply.github.com>
2018-10-20 02:36:39 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,7 +12,7 @@
          
         var patient = smart.patient;
         var pt = patient.read();
-        alert(JSON.stringify(pt));
+   
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
