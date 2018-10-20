1fd9fa43e2466fabfac094713ae667b1a827f8fd
spatangi <44306230+spatangi@users.noreply.github.com>
2018-10-20 02:31:23 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -9,6 +9,7 @@
 
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
+        alert(smart.patient); 
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
